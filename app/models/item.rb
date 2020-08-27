e918d8f75bbf3780d7f3e484797f4072b4b10e6b
tomoyasuito <shockeye.p.p.p@gmal.com>
2020-08-27 19:30:01 +0900

    カテゴリー選択の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :status, :postage, :shipping_charge, :area
+
+  #空の投稿を保存できないようにする
+  validates :name, :info, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category, :status, :postage, :shipping_charge, :area, numericality: { other_than: 1 } 
 end
