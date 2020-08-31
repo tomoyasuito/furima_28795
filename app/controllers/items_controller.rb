52d5a9a57902dead07f7f3c55e4da8b8de5e4832
tomoyasuito <shockeye.p.p.p@gmal.com>
2020-08-31 16:07:37 +0900

    商品一覧表示実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,17 @@
 class ItemsController < ApplicationController
 
   def index
+    @item = Item.all
   end
 
   def new
     @item = Item.new
+  end
+
+  def show
+  end
+
+  def destoroy
   end
 
   def create
