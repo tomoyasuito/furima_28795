a42e46e83f47890a0c6792f6cd10a64b279cf543
tomoyasuito <shockeye.p.p.p@gmal.com>
2020-08-31 21:47:23 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,8 +14,11 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if item.destroy
+       redirect_to root_path
+    else
+      render :show
+    end
   end
 
   def create
